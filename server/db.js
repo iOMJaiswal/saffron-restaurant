524a83520edfa40be6fe82adfe8bc28a5eb0a112
OM JAISWAL <jaiswalom268@gmail.com>
2026-03-28 02:57:27 +0530

    fix: build script, ready for vercel

--- server/db.js
+++ server/db.js
@@ -1,6 +1,11 @@
 import postgres from 'postgres';
 import 'dotenv/config';
 
-const sql = postgres(process.env.DATABASE_URL);
+const sql = postgres(process.env.DATABASE_URL, {
+  ssl: 'require',
+  max: 10,
+  idle_timeout: 20,
+  connect_timeout: 10,
+});
 
 export default sql;
